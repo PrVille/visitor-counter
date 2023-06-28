fc27ace2bc89581d6425e6b417df065deacf7cc5
ville <ville.prami@aalto.fi>
2023-06-28 11:37:33 +0300

    reset key

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const cors = require('cors')
 const client = require("./client")
 
 const app = express()
 const PORT = 3001
+
+app.use(cors())
 
 app.get("/api/:key", async (req, res) => {
   const value = await client.get(req.params.key)
@@ -20,7 +23,7 @@ app.post("/api/register/:key", async (req, res) => {
   if (!!value) {
     return res.status(401).send(`Key "${req.params.key}" already exists!`)
   }
-  
+
   await client.set(req.params.key, 0)
   res.send(`Registered ${req.params.key}`)
 })
@@ -28,6 +31,11 @@ app.post("/api/register/:key", async (req, res) => {
 app.post("/api/increment/:key", async (req, res) => {
   await client.incr(req.params.key)
   res.send(`Incremented ${req.params.key}`)
+})
+
+app.post("/api/reset/:key", async (req, res) => {
+  await client.set(req.params.key, 0)
+  res.send(`Reset ${req.params.key} to 0`)
 })
 
 const start = async () => {
